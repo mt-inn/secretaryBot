888a1bf3f385df9c79e8fb4372b76f70ac45d7e3
bakedMochi <ryua4ymnk@gmail.com>
2025-05-12 00:10:15 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,19 +5,16 @@ import re
 import discord
 from discord import app_commands
 from discord.ext import commands, tasks
-from discord.ui import Select, View
+from discord.ui import Select, ChannelSelect, View
 import datetime
 from dotenv import load_dotenv
 from core import secretaryCalendarCore as scc
 from core import secretaryReactCore as src
-import tracemalloc
 
-tracemalloc.start()
 load_dotenv()
 TOKEN=os.getenv("DISCORD_TOKEN")
 
 rem = scc.Reminder()
-rem.start_reminder_loop()
 client = rem.client
 tree = app_commands.CommandTree(client)
 
@@ -39,7 +36,7 @@ def str2dt(text:str):
     dt = datetime.datetime(*l)
     return dt
 
-class SelectView(View):
+class selectRemindView(View):
     @discord.ui.select(
         cls=Select,
         placeholder="",
@@ -47,27 +44,41 @@ class SelectView(View):
     )]
     )
     async def selectMenu(self, ctx: discord.Interaction, select: Select):
-        await ctx.response.send_message("送信先を決定しました。")
+        await ctx.response.send_message("送信先を決定しました。", ephemeral=True)
         if select.values[0] == 'ch':
             await rem.configAddres(typeInput="ch",addres=ctx.channel_id)
         else:
             await rem.configAddres(typeInput="dm", addres=ctx.user.id)
+class selectTalkView(View):
+    @discord.ui.select(
+        cls=ChannelSelect
+    )
+    async def selectMenu(self, ctx: discord.Interaction, select: Select):
+        src.React.reactCh = (select.values[0]).id
+        await ctx.response.send_message("送信先を決定しました。", ephemeral=True)
 @tree.command(name="test",description="テストコマンドです。")
 @app_commands.describe(
     text="Text to say hello." # 引数名=説明
 )
 async def test_command(ctx: discord.Interaction, text:str):
-    await ctx.response.send_message(f"てすと！\nprovided word: {text}",ephemeral=True)#ephemeral=True→「これらはあなただけに表示されています」
+    channel = client.get_channel(src.React.reactCh)
+    await channel.send(f"てすと！\nprovided word: {text}")#ephemeral=True→「これらはあなただけに表示されています」
+    return
 @tree.command(name="displayconfig",description="設定を見せる")
 async def command_displayconfig(ctx: discord.Interaction):
-    await ctx.channel.send(f"{rem.schedule}\n\n{rem.addres}")
+    await ctx.response.send_message(f"{rem.schedule}\n\n{rem.addres}",ephemeral=True)
     return
 #ここからbotのメイン機能たち
-@tree.command(name="config", description="botの設定をします。")
-async def command_config(ctx: discord.Interaction):
-    view = SelectView()
-    await ctx.response.send_message("リマインドの送信場所を設定", view=view)
-    
+@tree.command(name="configremind", description="リマインドの送信先を設定をします。")
+async def command_configremind(ctx: discord.Interaction):
+    view = selectRemindView()
+    await ctx.response.send_message("リマインドの送信場所を設定", view=view, ephemeral=True)
+    return
+@tree.command(name="configtalk", description="会話の場所を設定をします。")
+async def command_configtalk(ctx: discord.Interaction):
+    view = selectTalkView()
+    await ctx.response.send_message("リマインドの送信場所を設定", view=view, ephemeral=True)
+    return
 @tree.command(name="add_event",description="用事を登録します。")
 @app_commands.describe(name="用事の名前(必須)")
 @app_commands.describe(date= "予定の日時")
@@ -103,8 +114,8 @@ async def command_addEvent(ctx: discord.Interaction, name:str, date:str, locatio
         "yearly":"毎年"
         }
         embed.add_field(name="繰り返し", value=f"{repeatList[repeat]}お知らせします。", inline=False)
-    await ctx.channel.send(content=f"<@{user}>\n",embed=embed)
-
+    await ctx.response.send_message(content=f"<@{user}>\n",embed=embed,ephemeral=True)
+    return
 @tree.command(name="delete_event",description="用事を消します。")
 @app_commands.describe(name="削除したい用事の名前")
 @app_commands.describe(date="予定を開けたい日時")
@@ -117,7 +128,8 @@ async def command_deleteEvent(ctx: discord.Interaction, name:str=None, date:str=
     for e in deleted:
         detail += f"* {e['name']}({e['date'].strftime('%Y年%m月%d日 %H時%M分')})\n"
     embed.add_field(name="削除した予定の詳細", value=f"{detail}", inline=False)
-    ctx.channel.send(content=f"<@{user}>\n",embed=embed)
+    await ctx.response.send_message(content=f"<@{user}>\n",embed=embed,ephemeral=True)
+    return
 
 @tree.command(name="update_event",description="用事の詳細を変更します。")
 @app_commands.describe(old_name="変更したい予定の名前")
@@ -167,9 +179,10 @@ async def command_updateEvent(ctx: discord.Interaction, old_name:str, old_date:s
             content += f"**繰り返し:** {repeatList[new_repeat]}お知らせします。"
 
         embed.add_field(name="変更内容", value=f"{content}", inline=False)
-        ctx.channel.send(content=f"<@{user}>", embed=embed)
+        await ctx.response.send_message(content=f"<@{user}>", embed=embed,ephemeral=True)
     else:
-        ctx.response.send_message(content="予定の変更に失敗しました！\n入力内容をお確かめください。")
+        await ctx.response.send_message(content="予定の変更に失敗しました！\n入力内容をお確かめください。", ephemeral=True)
+    return
 
 
 @tree.command(name="list_events",description="予定一覧をリストとして取得します。")
@@ -186,7 +199,8 @@ async def command_listEvents(ctx: discord.Interaction,date:str=None):
         desc = "# 予定一覧\n"
         for e in eventList:
             events += f"{e['name']} ({e['date_time'].strftime('%Y年%m月%d日(%H時%M分)')})\n"
-    ctx.channel.send(content=f"<@{user}>\n{desc}{events}")
+    await ctx.response.send_message(content=f"<@{user}>\n{desc}{events}",ephemeral=True)
+    return
 
 @tree.command(name="remind_after",description="指定した期間の後でリマインドします。")
 @app_commands.describe(name="用事の名前(必須)")
@@ -214,5 +228,6 @@ async def command_remindAfter(ctx: discord.Interaction, name:str, delay:str, loc
 async def on_ready():
     print("起動完了")
     await tree.sync()#スラッシュコマンドを同期
+    # rem.start_reminder_loop()
 
 client.run(TOKEN)
